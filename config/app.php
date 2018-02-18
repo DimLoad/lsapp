17308c21388120581e14eda8e739d86f6755befd
Liviu <matliviu89@yahoo.com>
2018-02-18 16:43:52 +0200

    Forms & Saving Data

--- config/app.php
+++ config/app.php
@@ -150,7 +150,8 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Collective\Html\HtmlServiceProvider::class,
+        Unisharp\Ckeditor\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -209,6 +210,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<a href="/posts" class="btn btn-secondary">Go back</a>
+<a href="/posts" class="btn btn-secondary" style="margin-bottom: 20px">Go back</a>
     <h1>{{$post->title}}</h1>
     <div>
-        {{$post->body}}
+        {!!$post->body!!}
     </div>
     <hr>
     <small>Written on {{$post->created_at}}</small>
